2944ede950fe512026d0a7fcc03c9f8e9fb51828
lizwiz13 <demiankaras@gmail.com>
2024-01-15 00:29:50 +0100

    Added str method and total ordering for Thunks.

--- thunk.py
+++ thunk.py
@@ -2,6 +2,7 @@
 
 from singleton import Singleton
 from typing import TypeVar, Generic, Callable
+from functools import total_ordering
 
 # Not-Evaluated singleton class
 class NotEvaluated(metaclass=Singleton): pass
@@ -9,15 +10,42 @@ _NE = NotEvaluated()
 
 _T = TypeVar('_T')
 
+@total_ordering
 class Thunk(Generic[_T]):
     susp: Callable[[], _T]
     memo: NotEvaluated | _T
 
-    def __init__(self, f: Callable[[], _T]):
-        self.susp = f
+    def __init__(self, f: Callable[..., _T], *args, **kwargs):
+        # the if-else distinction decreases overhead from lambda in case of 0-parameter functions
+        if args or kwargs:
+            self.susp = lambda: f(*args, **kwargs)
+        else:
+            self.susp = f
         self.memo = _NE
     
     def __call__(self):
         if self.memo is _NE:
             self.memo = self.susp()
         return self.memo
+    
+    # repr still shows the thunk, but for readability it is better to evaluate the thunk
+    def __str__(self):
+        return str(self())
+    
+    def __eq__(self, other):
+        return self() == other()
+    
+    def __lt__(self, other):
+        return self() < other()
+    
+
+# for testing purposes only
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n < 0:
+        return -fib(n+1) + fib(n+2)
+    else:
+        return fib(n-1) + fib(n-2)
