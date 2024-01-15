56d1d4452d231bf9ccb0d88de20df9b29f9994ba
lizwiz13 <demiankaras@gmail.com>
2024-01-15 01:08:31 +0100

    Added more Thunk functions. Added force and const functions. Changed Thunk.__init__ so that additional args and kwargs values can     also be thunks.

--- thunk.py
+++ thunk.py
@@ -16,28 +16,73 @@ class Thunk(Generic[_T]):
     memo: NotEvaluated | _T
 
     def __init__(self, f: Callable[..., _T], *args, **kwargs):
-        # the if-else distinction decreases overhead from lambda in case of 0-parameter functions
+        # the if-else distinction decreases overhead from lambda and forcing in case of 0-parameter functions
         if args or kwargs:
-            self.susp = lambda: f(*args, **kwargs)
+            self.susp = lambda: f(*map(force, args), **{k: force(v) for k, v in kwargs.items()})
         else:
             self.susp = f
         self.memo = _NE
     
-    def __call__(self):
+    def __call__(self) -> _T:
         if self.memo is _NE:
             self.memo = self.susp()
-        return self.memo
-    
+        return self.memo # type: ignore
+
     # repr still shows the thunk, but for readability it is better to evaluate the thunk
     def __str__(self):
-        return str(self())
-    
+        return str(force(self))
+
     def __eq__(self, other):
-        return self() == other()
+        return force(self) == force(other)
     
     def __lt__(self, other):
-        return self() < other()
+        return force(self) < force(other)
     
+    def __hash__(self):
+        return hash(force(self))
+    
+    def __bool__(self):
+        return bool(force(self))
+
+    def __add__(self, other):
+        return Thunk(lambda: force(self) + force(other))
+    
+    def __sub__(self, other):
+        return Thunk(lambda: force(self) - force(other))
+    
+    def __mul__(self, other):
+        return Thunk(lambda: force(self) * force(other))
+    
+    def __matmul__(self, other):
+        return Thunk(lambda: self() @ other())
+    
+    def __truediv__(self, other):
+        return Thunk(lambda: self() / other())
+    
+    def __floordiv__(self, other):
+        return Thunk(lambda: self() // other())
+    
+    def __mod__(self, other):
+        return Thunk(lambda: self() % other())
+    
+    def __divmod__(self, other):
+        return Thunk(lambda: divmod(self(), other()))
+    
+    def __pow__(self, other, modulo=None):
+        return Thunk(lambda: pow(self, other, modulo))
+
+
+# using force instead of __call__ is safer and allows binary operations between Thunks and other types
+def force(x: Thunk[_T] | _T) -> _T:
+    if isinstance(x, Thunk):
+        return x()
+    return x
+
+
+def const(x: _T) -> Thunk[_T]:
+    'Shortcut for creating Thunk with an already evaluated object.'
+    return Thunk(lambda: x)
+
 
 # for testing purposes only
 def fib(n):
