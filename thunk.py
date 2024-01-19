80f3df4003e9fb57c6b55a3293c98f697a092b2a
lizwiz13 <demiankaras@gmail.com>
2024-01-19 23:43:52 +0100

    Change Thunk.__init__ and Thunk.__call__. Added args and kwargs as Thunk object variables. This removes the     necessity to make lambdas on __init__ call. Thunk.__memo__ is now a cached_property. This makes it faster and     reduces the amount of code in __call__     (which is now just a shortcut to access memo variable).

--- thunk.py
+++ thunk.py
@@ -1,101 +1,74 @@
 'Thunks for lazy evaluation with memoization'
 
 from typing import TypeVar, Generic, Callable
-from functools import total_ordering, wraps
+from functools import total_ordering, wraps, cached_property
 
 _T = TypeVar('_T')
 
 @total_ordering
 class Thunk(Generic[_T]):
-    susp: Callable[[], _T]
-    memo: _T
+    susp: Callable[..., _T]
+    args: tuple
+    kwargs: dict
 
-    def __init__(self, f: Callable[..., _T], *args, **kwargs):
+    @cached_property
+    def memo(self) -> _T:
+        return self.susp(*map(force, self.args), **{k: force(v) for k, v in self.kwargs.items()})
+
+    def __init__(self, func: Callable[..., _T], *args, **kwargs):
         # the if-else distinction decreases overhead from lambda and forcing in case of 0-parameter functions
-        if args or kwargs:
-            self.susp = lambda: f(*map(force, args), **{k: force(v) for k, v in kwargs.items()})
-        else:
-            self.susp = f
+        self.susp = func
+        self.args = args
+        self.kwargs = kwargs
     
-    def __call__(self) -> _T:
-        try:
-            return self.memo
-        except AttributeError as e:
-            if hasattr(self, 'memo'):
-                raise e
-            self.memo = self.susp()
-            return self.memo
-
+    def __call__(self) -> _T: return self.memo
 
     # repr still shows the thunk, but for readability it is better to evaluate the thunk
-    def __str__(self):
-        return str(force(self))
+    def __str__(self): return str(force(self))
 
-    def __eq__(self, other):
-        return force(self) == force(other)
+    def __eq__(self, other): return force(self) == force(other)
     
-    def __lt__(self, other):
-        return force(self) < force(other)
+    def __lt__(self, other): return force(self) < force(other)
     
-    def __hash__(self):
-        return hash(force(self))
+    def __hash__(self): return hash(force(self))
     
-    def __bool__(self):
-        return bool(force(self))
+    def __bool__(self): return bool(force(self))
 
-    def __add__(self, other):
-        return Thunk(lambda x, y: x + y, self, other)
+    def __add__(self, other): return Thunk(lambda x, y: x + y, self, other)
     
-    def __sub__(self, other):
-        return Thunk(lambda x, y: x - y, self, other)
+    def __sub__(self, other): return Thunk(lambda x, y: x - y, self, other)
     
-    def __mul__(self, other):
-        return Thunk(lambda x, y: x * y, self, other)
+    def __mul__(self, other): return Thunk(lambda x, y: x * y, self, other)
     
-    def __matmul__(self, other):
-        return Thunk(lambda x, y: x @ y, self, other)
+    def __matmul__(self, other): return Thunk(lambda x, y: x @ y, self, other)
     
-    def __truediv__(self, other):
-        return Thunk(lambda x, y: x / y, self, other)
+    def __truediv__(self, other): return Thunk(lambda x, y: x / y, self, other)
     
-    def __floordiv__(self, other):
-        return Thunk(lambda x, y: x // y, self, other)
+    def __floordiv__(self, other): return Thunk(lambda x, y: x // y, self, other)
     
-    def __mod__(self, other):
-        return Thunk(lambda x, y: x % y, self, other)
+    def __mod__(self, other): return Thunk(lambda x, y: x % y, self, other)
     
-    def __divmod__(self, other):
-        return Thunk(divmod, self, other)
+    def __divmod__(self, other): return Thunk(divmod, self, other)
     
-    def __pow__(self, other, modulo=None):
-        return Thunk(pow, self, other, modulo)
+    def __pow__(self, other, modulo=None): return Thunk(pow, self, other, modulo)
     
-    def __lshift__(self, other):
-        return Thunk(lambda x, y: x << y, self, other)
+    def __lshift__(self, other): return Thunk(lambda x, y: x << y, self, other)
     
-    def __rshift__(self, other):
-        return Thunk(lambda x, y: x >> y, self, other)
+    def __rshift__(self, other): return Thunk(lambda x, y: x >> y, self, other)
     
-    def __and__(self, other):
-        return Thunk(lambda x, y: x & y, self, other)
+    def __and__(self, other): return Thunk(lambda x, y: x & y, self, other)
     
-    def __xor__(self, other):
-        return Thunk(lambda x, y: x ^ y, self, other)
+    def __xor__(self, other): return Thunk(lambda x, y: x ^ y, self, other)
     
-    def __or__(self, other):
-        return Thunk(lambda x, y: x | y, self, other)
+    def __or__(self, other): return Thunk(lambda x, y: x | y, self, other)
     
-    def __neg__(self):
-        return Thunk(lambda x: -x, self)
+    def __neg__(self): return Thunk(lambda x: -x, self)
     
-    def __pos__(self):
-        return Thunk(lambda x: +x, self)
+    def __pos__(self): return Thunk(lambda x: +x, self)
     
-    def __abs__(self):
-        return Thunk(abs, self)
+    def __abs__(self): return Thunk(abs, self)
     
-    def __invert__(self):
-        return Thunk(lambda x: ~x, self)
+    def __invert__(self): return Thunk(lambda x: ~x, self)
 
 
 _LazyT = _T | Thunk['_LazyT[_T]']
@@ -117,7 +90,7 @@ def const(x: _T) -> Thunk[_T]:
 
 
 def lazy(f: Callable[..., _T]) -> Callable[..., Thunk[_T]]:
-
+    'Decorator for functions. Changes the output to Thunk, so that the actual result of the function may be evaluated easily.'
     @wraps(f)
     def wrapper(*args, **kwargs):
         return Thunk(lambda: force(f(*args, **kwargs)))
